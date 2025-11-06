a8ee8d2303c732a973aed97d3cbcf05c2beb7c66
Steven Spasbo <stevenspasbo@gmail.com>
2025-11-06 00:55:31 -0800

    Initial commit

---
+++ src/main/java/com/spasbo/mlb/MlbDataApplication.java
@@ -0,0 +1,13 @@
+package com.spasbo.mlb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spasbo/mlb/MlbDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spasbo.mlb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
