193712220bf896e3db3d97b84ec86719c929610c
Steven Spasbo <stevenspasbo@gmail.com>
2025-11-06 00:58:58 -0800

    Added CassandraConfiguration.java

---
+++ src/main/java/com/spasbo/mlb/configuration/CassandraConfiguration.java
@@ -0,0 +1,44 @@
+package com.spasbo.mlb.configuration;
+
+import java.util.List;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.DataCenterReplication;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
+import org.springframework.data.cassandra.core.cql.keyspace.SpecificationBuilder;
+
+@Getter
+@Configuration
+public class CassandraConfiguration extends AbstractCassandraConfiguration {
+
+  @Value(value = "${spring.cassandra.port}")
+  private int port;
+
+  @Value(value = "${spring.cassandra.contact-points}")
+  private String contactPoints;
+
+  @Value(value = "${spring.cassandra.keyspace-name}")
+  private String keyspaceName;
+
+  @Value(value = "${spring.cassandra.local-datacenter}")
+  private String localDataCenter;
+
+  @Value(value = "${spring.cassandra.schema-action}")
+  private SchemaAction schemaAction;
+
+  @Override
+  protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+    CreateKeyspaceSpecification specification = SpecificationBuilder.createKeyspace(keyspaceName)
+        .with(KeyspaceOption.DURABLE_WRITES, true)
+        .withSimpleReplication()
+        .withNetworkReplication(DataCenterReplication.of(localDataCenter, 1))
+        .ifNotExists();
+
+    return List.of(specification);
+  }
+
+}
