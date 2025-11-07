f18f767233a5e6ad067196c828c11ded3323eabf
Steven Spasbo <stevenspasbo@gmail.com>
2025-11-06 19:57:03 -0800

    Updated formatting of Game POJO

--- src/main/java/com/spasbo/mlb/model/Game.java
+++ src/main/java/com/spasbo/mlb/model/Game.java
@@ -2,6 +2,16 @@ package com.spasbo.mlb.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.time.Instant;
+import java.time.LocalDate;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Game(int gamePk, String link, Teams teams, Instant gameDate) {}
+public record Game(
+    int gamePk,
+    String link,
+    Teams teams,
+    Instant gameDate,
+    LocalDate officialDate,
+    GameStatus
+    status,
+    Venue venue
+) {}
